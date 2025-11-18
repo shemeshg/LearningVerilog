d5d719db2d9862c179974a7e1297eef65b1f32aa
shemeshg <gili.shemesh@gmail.com>
2025-11-18 10:25:06 +0200

    commit

--- icarus_gettings_started/cli/run.ts
+++ icarus_gettings_started/cli/run.ts
@@ -9,12 +9,13 @@ const binaryFile = "out"
 const topModule = "hello"
 const inFiles=[
     "../hdl/types_pkg.sv",
+    "../hdl/select_action.sv",
     "../hdl/hello.sv"            
 ]
 await $`mkdir -p  ../build`
 cd('../build')
 await $$debug`rm -f ${binaryFile}`
-await $$debug`iverilog -g2012 -o ${binaryFile} -s ${topModule}  ${inFiles}`
+await $$debug`iverilog -g2012 -o ${binaryFile} -s ${topModule} -I ../hdl ${inFiles}`
 await $$debug`vvp ${binaryFile}`
 
 exit(0)
--- icarus_gettings_started/hdl/hello.sv
+++ icarus_gettings_started/hdl/hello.sv
@@ -1,14 +1,22 @@
+
 module hello;
+  event updated;
   import types_pkg::*;
+  int result;
+  
+  always_comb begin
+    result = add(5, 3);
+    -> updated; 
+  end
+
 
   task run_simulation;
-    int result;
-    $display("Hello, World parameter value is: %0d", types_pkg::BITS);
-    result = add(5, 3);
+    $display("Hello, World parameter value is: %0d", types_pkg::BITS);    
     $display("Result: %0d", result);
   endtask
 
   initial begin
+    @updated; 
     run_simulation;
     $finish;
   end
