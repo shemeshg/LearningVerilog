fd6935acb588bca68472edcabedd82fb7dbd5b23
shemeshg <gili.shemesh@gmail.com>
2025-11-18 10:36:30 +0200

    commit

--- icarus_gettings_started/hdl/hello.sv
+++ icarus_gettings_started/hdl/hello.sv
@@ -2,8 +2,21 @@
 module hello;
   event updated;
   import types_pkg::*;
-  int result;
+
   
+  localparam BITS = 16;
+
+
+  int result;
+  word_t LED_TB;
+  word_t SW_TB;
+  opr_mode_t            SELECTOR_TB;
+
+  select_action #() select_action_inst (
+      .SELECTOR(SELECTOR_TB),
+      .SW(SW_TB),
+      .LED(LED_TB)
+  );
   always_comb begin
     result = add(5, 3);
     -> updated; 
@@ -13,9 +26,12 @@ module hello;
   task run_simulation;
     $display("Hello, World parameter value is: %0d", types_pkg::BITS);    
     $display("Result: %0d", result);
+    $display("LED_TB: %0d", LED_TB);
   endtask
 
   initial begin
+    SELECTOR_TB = ADD;
+    SW_TB = '0;
     @updated; 
     run_simulation;
     $finish;
