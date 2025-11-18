5efbbcd3c897a676cf730ef08b328aa2ee2862b5
shemeshg <gili.shemesh@gmail.com>
2025-11-18 22:47:30 +0200

    commit

--- Youtube/ch03/tb/tb_example.sv
+++ Youtube/ch03/tb/tb_example.sv
@@ -1,23 +1,38 @@
 `timescale 1ns/10ps
 module tb_example;
+
+
+
 reg A,B,C,D,E,F; 
 wire Y;
 example DUT(A,B,C,D,E,F,Y);
 
+
+
+task run_simulation;
+    #4
+    $display ($time,"A=%b, B=%b, C=%b, D=%b, E=%b, F=%b, Y=%b",
+        A,B,C,D,E,F,Y);
+endtask
 initial begin
     $dumpfile("wave.vcd");
     $dumpvars (0,tb_example);
-    // Use $strobe instead of $monitor
+
+    A=1; B=0; C=0; D=1; E=0; F=0;
+    run_simulation;
+    
+    A=0; B=0; C=1; D=1; E=0; F=0;
+    run_simulation;
+    
+    A=1; C=0;
+
+    run_simulation;
+
+    F=1;
 
 
-    #5 A=1; B=0; C=0; D=1; E=0; F=0;
-    #5 $display ($time,"A=%b, B=%b, C=%b, D=%b, E=%b, F=%b, Y=%b",
-        A,B,C,D,E,F,Y);
-    #5 A=0; B=0; C=1; D=1; E=0; F=0;
-    #5 $display ($time,"A=%b, B=%b, C=%b, D=%b, E=%b, F=%b, Y=%b",
-        A,B,C,D,E,F,Y);
-    #5 A=1; C=0;
-    #5 F=1;
-    #5 $finish;
+    run_simulation;
+
+    $finish;
 end
 endmodule
