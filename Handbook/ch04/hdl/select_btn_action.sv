838b3b6fa68a3ba7de655b8a62dcac2a4ad0ed29
shemeshg <gili.shemesh@gmail.com>
2025-12-05 20:49:48 +0200

    commit

--- Handbook/ch04/hdl/select_btn_action.sv
+++ Handbook/ch04/hdl/select_btn_action.sv
@@ -2,6 +2,7 @@ module select_btn_action (
     input  word_t SW,
     output word_t LED,
     input  wire   CPU_RESETN,
+    input  wire   CLOCK,
     input  wire   BTNC,
     input  wire   BTNU,
     input  wire   BTNL,
@@ -17,15 +18,30 @@ module select_btn_action (
       .LED(LED)
   );
 
+  opr_mode_t rememberd_selected;
+  initial begin
+    rememberd_selected = RESET;
+  end
+
+  always_ff @(posedge CLOCK or posedge CPU_RESETN) begin
+    if (CPU_RESETN) begin
+      rememberd_selected <= RESET;
+    end else begin
+      case (1'b1)
+        BTNC: rememberd_selected <= MUL;
+        BTNU: rememberd_selected <= LEADING_ONES;
+        BTND: rememberd_selected <= COUNT_ONES;
+        BTNL: rememberd_selected <= ADD;
+        BTNR: rememberd_selected <= SUB;
+        RESET: rememberd_selected <= RESET;
+        //default: rememberd_selected <= RESET;
+      endcase
+      //$display("Time: %0t  shalom %b %b", $time,rememberd_selected, SELECTOR_TB);
+    end
+  end
+
   always_comb begin
-    case (1'b1)
-      BTNC: SELECTOR_TB = MUL;
-      BTNU: SELECTOR_TB = LEADING_ONES;
-      BTND: SELECTOR_TB = COUNT_ONES;
-      BTNL: SELECTOR_TB = ADD;
-      BTNR: SELECTOR_TB = SUB;
-      default: SELECTOR_TB = RESET;
-    endcase
+    SELECTOR_TB = rememberd_selected;
   end
 
 endmodule
--- Handbook/ch04/tb/tb_emulator.sv
+++ Handbook/ch04/tb/tb_emulator.sv
@@ -15,76 +15,76 @@ module tb_emulator;
 
   localparam BITS = 16;
 
-  word_t     LED_TB;
-  word_t     SW_TB;
+  word_t  LED_TB;
+  word_t  SW_TB;
 
 
-  logic      BTNC_TB;
-  logic      BTNU_TB;
-  logic      BTNL_TB;
-  logic      BTNR_TB;
-  logic      BTND_TB;
+  logic   BTNC_TB;
+  logic   BTNU_TB;
+  logic   BTNL_TB;
+  logic   BTNR_TB;
+  logic   BTND_TB;
 
   integer ret;
   select_btn_action #() select_btn_action_inst (
       .SW(SW_TB),
       .LED(LED_TB),
       .CPU_RESETN(rst),
+      .CLOCK(clk),
       .BTNC(BTNC_TB),
       .BTNU(BTNU_TB),
       .BTNL(BTNL_TB),
       .BTNR(BTNR_TB),
-      .BTND(BTND_TB)     
+      .BTND(BTND_TB)
   );
 
 
   int fdw_led;
   int fdr_sw;
-  
-  always @(posedge clk or posedge rst) begin
-    if (rst) begin
+
+  always @(posedge clk) begin
+
+    //#1000000000;
+    #10000000;
+    #10ns;
+
+    fdw_led = $fopen("/Volumes/RAM_Disk_4G/tmpFifo/myLeds", "w");
+    if (fdw_led) begin
+      $fwrite(fdw_led, "Time: %0t | SW_TB: %b | LED_TB: %b\n", $time, SW_TB, LED_TB);
+      $fclose(fdw_led);
     end else begin
-      //#1000000000;
-      #100000000;
-      #10ns;
-
-      fdw_led = $fopen("/Volumes/RAM_Disk_4G/tmpFifo/myLeds", "w");
-      if (fdw_led) begin
-        $fwrite(fdw_led, "Time: %0t | SW_TB: %b | LED_TB: %b\n", $time, SW_TB,LED_TB);
-        $fclose(fdw_led);
-      end else begin
-        $display("ERROR: Could not open file!");
-      end
-
-      fdr_sw = $fopen("/Volumes/RAM_Disk_4G/tmpFifo/mySw", "r");
-      if (fdr_sw == 0) begin
-        $display("ERROR: could not open file");
-      end else begin
-        while (!$feof(
-            fdr_sw
-        )) begin
-          ret = $fscanf(fdr_sw, "%b", SW_TB);
-          if (ret == 1) begin
-          end
-        end
-        $fclose(fdr_sw);
-      end
-
-      fdr_sw = $fopen("/Volumes/RAM_Disk_4G/tmpFifo/myBtns", "r");
-      if (fdr_sw == 0) begin
-        $display("ERROR: could not open file");
-      end else begin
-        while (!$feof(
-            fdr_sw
-        )) begin
-          ret = $fscanf(fdr_sw, "%b %b %b %b %b %b", rst, BTNU_TB, BTNL_TB, BTNC_TB, BTNR_TB, BTND_TB);
-          if (ret == 1) begin
-          end
-        end
-        $fclose(fdr_sw);
-      end
-
+      $display("ERROR: Could not open file!");
     end
+
+    fdr_sw = $fopen("/Volumes/RAM_Disk_4G/tmpFifo/mySw", "r");
+    if (fdr_sw == 0) begin
+      $display("ERROR: could not open file");
+    end else begin
+      while (!$feof(
+          fdr_sw
+      )) begin
+        ret = $fscanf(fdr_sw, "%b", SW_TB);
+        if (ret == 1) begin
+        end
+      end
+      $fclose(fdr_sw);
+    end
+
+    fdr_sw = $fopen("/Volumes/RAM_Disk_4G/tmpFifo/myBtns", "r");
+    if (fdr_sw == 0) begin
+      $display("ERROR: could not open file");
+    end else begin
+      while (!$feof(
+          fdr_sw
+      )) begin
+        ret =
+            $fscanf(fdr_sw, "%b %b %b %b %b %b", rst, BTNU_TB, BTNL_TB, BTNC_TB, BTNR_TB, BTND_TB);
+        if (ret == 1) begin
+        end
+      end
+      $fclose(fdr_sw);
+    end
+
   end
 
 
@@ -94,7 +94,7 @@ module tb_emulator;
 
     @(negedge rst);
 
-    SW_TB = 16'd0;
+    SW_TB   = 16'd0;
 
     BTNC_TB = 0;
     BTNU_TB = 0;
