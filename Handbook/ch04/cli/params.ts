af91d0961c1fd81b49b2b8f37f3f232d8b1b25b6
shemeshg <gili.shemesh@gmail.com>
2025-12-10 18:41:15 +0200

    commit

--- Handbook/ch04/cli/params.ts
+++ Handbook/ch04/cli/params.ts
@@ -5,7 +5,7 @@ export const inFiles=[
     "../hdl/types_pkg.sv",
     "../hdl/dec_cat_map.sv",
     "../hdl/bin_to_bcd.sv",
-    "../hdl/seg_display.sv",
+    "../hdl/seg_display_calc.sv",
     "../hdl/add_sub_mult.sv",
     "../hdl/select_action.sv",
     "../hdl/select_btn_action.sv",
--- Handbook/ch04/hdl/seg_display_calc.sv
+++ Handbook/ch04/hdl/seg_display_calc.sv
@@ -2,8 +2,15 @@ import types_pkg::*;
 
 module seg_display (
     input display_t display,
+    input wire CLK100MHZ,
     input wire CPU_RESET,
-    input wire CLOCK,
+    input word_t SW,
+    output word_t  LED,
+    input wire                     BTNC,
+    input wire                     BTNU,
+    input wire                     BTNL,
+    input wire                     BTNR,
+    input wire                     BTND,      
     output word_t AN,
     output wire   CA,
     output wire   CB,
