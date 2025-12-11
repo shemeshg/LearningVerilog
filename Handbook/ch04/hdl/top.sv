9ad833205aa8403383525f41aea1e22b9fe78a47
shemeshg <gili.shemesh@gmail.com>
2025-12-11 03:16:45 +0200

    commit

--- Handbook/ch04/hdl/top.sv
+++ Handbook/ch04/hdl/top.sv
@@ -3,31 +3,36 @@
 import types_pkg::*;
 
 
-module top (
-    input wire CLK100MHZ,
-    input wire CPU_RESETN,
-    input wire [15:0] SW,
-    input wire [7:0] AN,
-    output logic signed [BITS-1:0]  LED,
-    input wire                     BTNC,
-    input wire                     BTNU,
-    input wire                     BTNL,
-    input wire                     BTNR,
-    input wire                     BTND,
-    input wire  CA,
-    input wire  CB,
-    input wire  CC,
-    input wire  CD,
-    input wire  CE,
-    input wire  CF,
-    input wire  CG,
-    input wire  DP    
+module top #(
+    parameter NUM_SEGMENTS = 8,
+    parameter CLK_PER      = 10,   // Clock period in ns
+    parameter REFR_RATE    = 1000  // Refresh rate in Hz
+) (
+    input  wire                    CLK100MHZ,
+    input  wire                    CPU_RESETN,
+    input  wire         [    15:0] SW,
+    output logic signed [BITS-1:0] LED,
+    input  wire                    BTNC,
+    input  wire                    BTNU,
+    input  wire                    BTNL,
+    input  wire                    BTNR,
+    input  wire                    BTND,
+    output  logic         [     7:0] AN,
+    output  logic                    CA,
+    output  logic                    CB,
+    output  logic                    CC,
+    output  logic                    CD,
+    output  logic                    CE,
+    output  logic                    CF,
+    output  logic                    CG,
+    output  logic                    DP
 );
 
   wire rst;
   assign rst = ~CPU_RESETN;
   wire clk;
   assign clk = CLK100MHZ;
+  localparam INTERVAL = int'(100000000 / (CLK_PER * REFR_RATE));
 
   logic  BTNC_DEB;
   logic  BTNU_DEB;
@@ -106,6 +111,29 @@ module top (
       .BTND(BTND_DEB)
   );
 
-  
 
+  logic [7:0] cathode;
+  assign {DP, CG, CF, CE, CD, CC, CB, CA} = cathode;
+
+  logic [    $clog2(INTERVAL)-1:0] refresh_count;
+  logic [$clog2(NUM_SEGMENTS)-1:0] anode_count;
+  initial begin
+    refresh_count = '0;
+    anode_count   = '0;
+  end
+
+  always @(posedge clk) begin
+    if (rst) begin
+      refresh_count <= '0;
+      anode_count   <= '0;
+    end
+    if (refresh_count == INTERVAL) begin
+      refresh_count <= '0;
+      anode_count   <= anode_count + 1'b1;
+    end else refresh_count <= refresh_count + 1'b1;
+    AN              <= '1;
+    AN[anode_count] <= '0;
+    cathode            <= display[anode_count*8+:8];
+
+  end
 endmodule
